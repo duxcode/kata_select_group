a30f76533f7502dd0d3ddf1d83033d596af79e7c
Fernando Portilla <fernandoportilla1@gmail.com>
2017-05-13 00:45:19 -0500

    first commit

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -13,6 +13,34 @@ angular.module('Group')
             return res
         }
 
+        $scope.beach = function(data) {
+            var person_beach = []
+            var data1 = angular.copy(data)
+            data.map(function(group) {
+                console.log(group)
+                group.map(function(person) {
+
+                    // var person_rep = []
+                    // data1.map(function(group_validate) {
+                    //     if (person > 0) {
+                    //         var cond = group_validate.indexOf(person) >= 0 ? true : false
+                    //         if (cond) {
+                    //             person_rep.push(person)
+                    //         }
+                    //     }
+                    // })
+
+                    // if (person_rep.length > 1) {
+                    //     if (person_beach.indexOf(person) < 0) {
+                    //         person_beach.push(person)
+                    //     }
+                    // }
+                })
+            })
+
+            return person_beach
+        }
+
     })
     .config(function($routeProvider) {
         $routeProvider
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -2,62 +2,91 @@
 
 describe('Controller: select group', function() {
 
-  beforeEach(module('Group'));
+    beforeEach(module('Group'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('group', {
-      $scope: scope
-    });
-  }));
-
-  describe('On instance', function() {
-    it('should set "controller_loaded" variable in scope', function() {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-  });
-
-  describe('On instance', function() {
-    it('scope function multiplication', function() {
-      var res = scope.multiplication([2, 2])
-      expect(res).toBe(4);
-
-      var res = scope.multiplication([2, 2, 2, 2])
-      expect(res).toBe(16);
-    });
-  });
-
-  describe('when going to /group', function() {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function($route, $location, $rootScope, $httpBackend) {
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('group', {
+            $scope: scope
+        });
     }));
 
-    afterEach(function() {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function() {
+        it('should set "controller_loaded" variable in scope', function() {
+            expect(scope.controller_loaded).toContain('loaded');
+        });
     });
 
-    it('should use minesweeper.html and controller', function() {
-      expect(route.current).toBeUndefined();
+    describe('On instance', function() {
+        it('scope function multiplication', function() {
+            var res = scope.multiplication([2, 2])
+            expect(res).toBe(4);
 
-      location.path('/group');
-
-      httpBackend.flush();
-
-      expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
-      expect(route.current.controller).toBe('group');
+            var res = scope.multiplication([2, 2, 2, 2])
+            expect(res).toBe(16);
+        });
     });
-  });
+
+    describe('On instance', function() {
+        it('scope function Beach', function() {
+            // var res = scope.beach([
+            //     ['1009', '2011'],
+            //     ['1017', '2011']
+            // ])
+
+            // expect(res).toEqual(['2011']);
+
+            // var res = scope.beach([
+            //     ['1009', '2000'],
+            //     ['1009', '2001'],
+            //     ['1002', '2002'],
+            //     ['1003', '2002']
+            // ])
+
+            expect(res).toEqual(['1009', '2002']);
+
+            var res = scope.beach([
+                ['1007', '2000'],
+                ['1000', '2000'],
+                ['1000', '2002'],
+                ['1006', '2002']
+            ])
+
+            expect(res).toEqual(['2000', '2002']);
+        });
+    });
+
+    describe('when going to /group', function() {
+
+        var route, location, rootScope, httpBackend;
+
+        beforeEach(inject(function($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
+
+            httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+        }));
+
+        afterEach(function() {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use minesweeper.html and controller', function() {
+            expect(route.current).toBeUndefined();
+
+            location.path('/group');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
+            expect(route.current.controller).toBe('group');
+        });
+    });
 
 });
